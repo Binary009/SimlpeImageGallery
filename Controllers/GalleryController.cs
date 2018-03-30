1b2b044db7841b7f541f2c0a96d72e5f9ac99f85
Sammie <samoski93@gmail.com>
2018-03-30 17:40:28 +0100

    Modified the using SimpleImageGallery Models

--- Controllers/GalleryController.cs
+++ Controllers/GalleryController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SimpleImageGallery.Data;
-using SimpleImageGallery.Models;s
+using SimpleImageGallery.Models;
 
 namespace SimpleImageGallery.Controllers
 {
